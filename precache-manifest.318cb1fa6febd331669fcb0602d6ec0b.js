17f19b2748a3b84773f2581ed1e1adf9c1ccdb05
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-08-17 17:11:45 +0300

    Updates

---
+++ precache-manifest.318cb1fa6febd331669fcb0602d6ec0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6558157d4f40e2d3c32b2e08926032",
+    "url": "/goit-react-hw-2-feedback/index.html"
+  },
+  {
+    "revision": "997dfcaee9a4ba35b1cf",
+    "url": "/goit-react-hw-2-feedback/static/css/main.c78796c4.chunk.css"
+  },
+  {
+    "revision": "60515829eabbf3a966f0",
+    "url": "/goit-react-hw-2-feedback/static/js/2.bf0da08d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-2-feedback/static/js/2.bf0da08d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997dfcaee9a4ba35b1cf",
+    "url": "/goit-react-hw-2-feedback/static/js/main.725656a4.chunk.js"
+  },
+  {
+    "revision": "fb690ee253ba7e3c7bed",
+    "url": "/goit-react-hw-2-feedback/static/js/runtime-main.e3b552f1.js"
+  }
+]);
